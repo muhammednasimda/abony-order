508c8da2f27eb1378eaf31a82b5179567d341a2b
muhammednasimda <muhammednasimda@gmail.com>
2021-03-17 09:28:50 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <Switch>
           <Route path="/addorder" component={AddOrder} />
           <Route path="/orderlist" component={OrderList} />
+          <Route path="/orderedit" component={OrderList} />
         </Switch>
       </Router>
     </>
