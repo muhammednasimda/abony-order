ca894de025a56723e40b7a78e6b87d1ffba8a951
hadi-a-azeez <hadikoduvally@gmail.com>
2021-04-26 16:44:38 +0530

    dashboard ui

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,12 +1,28 @@
-import { Input } from "@chakra-ui/input";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
 import React from "react";
 import SideBar from "../components/sideBar";
 import style from "./css/dashboard.module.scss";
+import { SearchIcon } from "@chakra-ui/icons";
+import { InputLeftElement } from "@chakra-ui/input";
 
 const Dashboard = () => {
   return (
     <div className={style.container}>
-      <SideBar />
+      <div style={{ width: "20%" }}>
+        <SideBar />
+      </div>
+      <InputGroup size="lg" p="2" alignSelf="flex-start" mt="8" w="25%">
+        <InputLeftElement width="4.5rem">
+          <SearchIcon mt="5" color="gray.400" />
+        </InputLeftElement>
+        <Input
+          pr="4.5rem"
+          placeholder="Search"
+          borderRadius="full"
+          borderColor="gray.100"
+          bg="gray.100"
+        />
+      </InputGroup>
     </div>
   );
 };
